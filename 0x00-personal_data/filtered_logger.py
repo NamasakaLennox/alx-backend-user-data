bcf05119af950299953491ad5c7f51a15bc041b2
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-11 23:51:53 +0300

    [new]: uses regex to mask data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+returns the log message obfuscated
+'''
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Uses regex to replace sensitive information
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}',
+                         message)
+
+    return message
