d36035a80603df8fe622f702736dd36e4374a650
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-16 22:21:00 +0300

    [new]: allow asterics

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,10 +13,28 @@ class Auth:
         """
         if path is None or excluded_paths is None:
             return True
-        path2 = path + '/'
+        l_path = len(path)
+        if l_path == 0:
+            return True
 
-        if path in excluded_paths or path2 in excluded_paths:
-            return False
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
