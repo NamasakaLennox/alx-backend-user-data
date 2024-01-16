9954569ec38aa3888b9ab538e3b4768349e5c1a0
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-16 21:05:00 +0300

    [new]: decoding a base64 encoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Basic Authentication
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+from binascii import Error
 
 
 class BasicAuth(Auth):
@@ -18,3 +20,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """decodes value of Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Error:
+            return None
