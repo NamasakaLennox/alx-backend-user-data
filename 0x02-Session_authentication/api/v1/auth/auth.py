86c8cd0d1f3038c22ee2275534e2ad27040cbfcc
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-17 16:14:31 +0300

    [new]: directory setup for new projects

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Manages API Authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """a class to manage api authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """check if path requires auth
+        """
+        if path is None or excluded_paths is None:
+            return True
+        l_path = len(path)
+        if l_path == 0:
+            return True
+
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """sets the auth header
+        """
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """checks the current user
+        """
+        return None
