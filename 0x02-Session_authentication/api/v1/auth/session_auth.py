de5a70206a903ce8b03899b6a779ca4568c09ac1
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-17 20:10:53 +0300

    [new]: create user object from cookie

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Implements a session auth
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -28,3 +29,11 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a user instance based on the cookie value
+        """
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+
+        return User.get(user_id)
