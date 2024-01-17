176d286d560d5160ea3854fb115f136261d16983
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-17 19:00:10 +0300

    [new]: implemented a basic session auth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,20 @@
 Implements a session auth
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """a class that manages a session auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a new session and generates session id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        ses_id = str(uuid4())
+        self.user_id_by_session_id[ses_id] = user_id
+
+        return ses_id
