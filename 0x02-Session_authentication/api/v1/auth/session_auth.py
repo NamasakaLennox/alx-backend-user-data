08161761ccd587dd61c4e162cfdd68d165e8c97c
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-17 17:02:48 +0300

    [new] implemented a session auth checker

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Implements a session auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """a class that manages a session auth
+    """
+    pass
