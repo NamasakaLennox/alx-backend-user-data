e268d78003773912a2952a9adbf5e10ef60f1f5f
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-17 19:11:07 +0300

    [new]: extracts user id from session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,11 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[ses_id] = user_id
 
         return ses_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Extracts a user id from the session id
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
