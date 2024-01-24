c2747db70f4f217567b3d9304f8a90a53135a490
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-24 18:58:06 +0300

    [fix]: implemented a new fix for the db class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""DB module for the users
+"""
+DB module that connects and intializes the database
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base, User
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import User
+from user import Base
 
 
 class DB:
@@ -17,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,31 +34,37 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database.
+        """
+        uses email and hased password to create a user, save
+        it to database and return it
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
-        self._session.commit()
+        self.__session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Finds a user by key worded arguments
         """
-        if not kwargs or not self.valid_query_args(**kwargs):
+        Filters for a user utilizing kwargs, and
+        returns it
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except Exception as e:
             raise InvalidRequestError
-
-        user = self.__session.query(User).filter_by(**kwargs).first()
-
-        if not user:
+        if user is None:
             raise NoResultFound
         return user
 
-    def valid_query_args(self, **kwargs) -> bool:
-        """Gets user table columns
+    def update_user(self, user_id: int, **kwargs) -> None:
         """
-        columns = User.__table__.columns.keys()
-        for key in kwargs.keys():
-            if key not in columns:
-                return False
-
-        return True
+        Locates user to update by id, updates user attributes
+        as passed in kwargs commits changes to database or
+        raises a ValueError if attribute not present
+        """
+        user = self.find_user_by(id=user_id)
+        for key, val in kwargs.items():
+            if getattr(user, str(key), 'None') == 'None':
+                raise ValueError
+            setattr(user, str(key), val)
+        self._session.commit()
