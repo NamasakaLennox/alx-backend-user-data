906e78e8bfc985ed603526c0126fbad60166a516
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-24 21:02:05 +0300

    [new]: a basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A basic flask app
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """default route for the app
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
