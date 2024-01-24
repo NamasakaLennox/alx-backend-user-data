c302d386665a319ccb1ac118c9f5bec9389c7e0f
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-24 20:53:39 +0300

    [new]: implemented an auth class that registers a new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,28 @@
 """Takes a password string and returns a hashed byte string
 """
 from bcrypt import gensalt, hashpw
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            password = _hash_password(password)
+
+            user = self._db.add_user(email, password)
+
+            return user
 
 
 def _hash_password(password: str) -> bytes:
