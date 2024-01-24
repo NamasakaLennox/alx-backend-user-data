7aae2dd1e7ad5703af48ce4660a44cc672c4ebd9
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-24 21:38:35 +0300

    [new]: checks if password is valid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Takes a password string and returns a hashed byte string
 """
-from bcrypt import gensalt, hashpw
+from bcrypt import checkpw, gensalt, hashpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -13,9 +13,13 @@ class Auth:
     """
 
     def __init__(self):
+        """Constructor method
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
+        """Registers a new user
+        """
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists')
@@ -25,6 +29,18 @@ class Auth:
             user = self._db.add_user(email, password)
 
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """checks if the login credentials are valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            password = password.encode('utf-8')
+            if checkpw(password, user.hashed_password):
+                return True
+            return False
+        except (NoResultFound, InvalidRequestError):
+            return False
 
 
 def _hash_password(password: str) -> bytes:
