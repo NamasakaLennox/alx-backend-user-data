921aa605e9307e2becb79eb6d09be8404ef2e482
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-24 23:39:49 +0300

    [new]: implemented a simple auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,5 +64,18 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile/', strict_slashes=False)
+def profile():
+    """gets profile information from users
+    """
+    session_id = request.cookies.get('session_id')
+
+    user = user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({'email': user.email}), 200
+
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
