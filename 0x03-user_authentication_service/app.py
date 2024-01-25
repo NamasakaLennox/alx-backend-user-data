2febb837e0d19fe8473b338726c198f4461c9019
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-25 19:24:54 +0300

    [new]: route that gets a user reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,5 +77,19 @@ def profile():
     abort(403)
 
 
+@app.route('/reset_password/', strict_slashes=False, methods=['POST'])
+def get_reset_password_token():
+    """get a reset token when requested by user
+    """
+    email = request.form.get('email')
+
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
